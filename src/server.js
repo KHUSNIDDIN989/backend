56ed920e73083ee5a2d8659f6ecbc40d989151c4
khusniddin <khusniddin989@gmail.com>
2022-08-11 10:25:16 +0500

    finshed

--- src/server.js
+++ src/server.js
@@ -1,12 +1,19 @@
 require("dotenv").config();
 const { ApolloServer } = require("apollo-server");
 const modules = require("./modules");
+const {
+  ApolloServerPluginLandingPageGraphQLPlayground,
+} = require("apollo-server-core");
+
 const PORT = process.env.PORT || 4004;
 
 const server = new ApolloServer({
   modules,
   introspection: true,
   playground: true,
+  csrfPrevention: true,
+  cache: "bounded",
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
 });
 
 server.listen(PORT).then(({ url }) => {
