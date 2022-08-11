8c8157afb18a4bc3c4ac8e9a7e457a57127a6633
khusniddin <khusniddin989@gmail.com>
2022-08-11 10:08:02 +0500

    finshed

--- src/server.js
+++ src/server.js
@@ -1,10 +1,14 @@
+require("dotenv").config();
 const { ApolloServer } = require("apollo-server");
 const modules = require("./modules");
+const PORT = process.env.PORT || 4004;
 
 const server = new ApolloServer({
   modules,
+  introspection: true,
+  playground: true,
 });
 
-server.listen().then(({ url }) => {
+server.listen(PORT).then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
 });
